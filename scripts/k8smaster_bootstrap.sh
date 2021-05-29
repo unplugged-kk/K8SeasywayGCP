a412edfafed8613335afec8b6ecb217f6679feae
unplugged-kk <kishore.behera2010@gmail.com>
2021-05-29 15:07:31 +0530

    updated script

--- scripts/k8smaster_bootstrap.sh
+++ scripts/k8smaster_bootstrap.sh
@@ -65,7 +65,6 @@ NODENAME=$(hostname -s)
 
 sudo kubeadm init --apiserver-advertise-address=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=10.241.0.0/16 --node-name $NODENAME --ignore-preflight-errors Swap
 
-
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
